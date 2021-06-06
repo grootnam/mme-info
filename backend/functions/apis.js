8be84cea86e4cebca055191ed88c8eebf33cd4a0
yu4321 <ryu.youngseok0512@gmail.com>
2021-06-07 07:11:58 +0900

    캘린더 페이지 계속

--- backend/functions/apis.js
+++ backend/functions/apis.js
@@ -73,26 +73,37 @@ exports.sendToken=functions.https.onRequest((request, response)=>{
 
 exports.postData =  functions.https.onRequest((request, response) => {
   return cors(request, response, () => {
-    var body = request.body;
-    console.log('postData get body : ',body)
+    // var body = request.body;
+    // console.log('postData get body : ',body)
+    // response.set('Access-Control-Allow-Origin', "*")
+    var body = JSON.parse(String(request.body))
     var indexName="";
     var collectionName="";
 
-    if(body.type == 'qna'){
+    //console.log('postdata get body type : ' + body["type"])
+
+    if(body["type"] == "qna"){
       indexName = "qna_last_index"
       collectionName = "qnas"
     }
-    else if(body.type == 'notice'){
+    else if(body["type"] == "notice"){
       indexName = "notice_last_index"
       collectionName = "notices"
     }
-    else if(body.type == 'schedule'){
+    else if(body["type"] == "schedule"){
       indexName="schedule_last_index"
       collectionName="schedules"
-      body["start"] = firestore.Timestamp.fromDate(body["start"])
-      body["end"] = firestore.Timestamp.fromDate(body["end"])
+      var data=body["data"]
+      var orgStart = data["start"]
+      var orgEnd= data["end"]
+      //console.log('org start and end: ',orgStart,orgEnd)
+      var dStart = new Date(orgStart)
+      var dEnd = new Date(orgEnd)
+      //console.log('dt start and end: ',dStart,dEnd)
+      data["start"] = firestore.Timestamp.fromDate(dStart)
+      data["end"] = firestore.Timestamp.fromDate(dEnd)
 
-      console.log('altered body : ',body)
+     // console.log('altered body : ',body)
     }
     else{
       response.status(405).send({
@@ -107,10 +118,11 @@ exports.postData =  functions.https.onRequest((request, response) => {
       var index = snap.data()[indexName];
       var dbRef = db.collection(collectionName);
 
-      var newData=body.data;
+      //console.log('got '+indexName+" : ",index);
+      var newData=body["data"];
       newData["index"] = index+1
       newData["timestamp"] = firestore.Timestamp.fromDate(new Date());
-      console.log('final new databody : ',newData)
+     // console.log('final new databody : ',newData)
       return dbRef
         .doc()
         .set(newData)
@@ -132,146 +144,45 @@ exports.postData =  functions.https.onRequest((request, response) => {
   });
 });
 
-// exports.postQna = functions.https.onRequest((request, response) => {
-//   cors(request, response, () => {
-//     var body = request.body;
-//     var getCurrentMetadataPromise = db
-//       .doc("metadata/PpmSaO7W089QJuMBY9Md")
-//       .get();
-//     getCurrentMetadataPromise.then((snap) => {
-//       var index = snap.data()["qna_last_index"];
-//       var qnaRef = db.collection("qnas");
-//       return qnaRef
-//         .doc()
-//         .set({
-//           index: index + 1,
-//           question: body.question,
-//           category: body.category,
-//           answer: null,
-//           timestamp: firestore.Timestamp.fromDate(new Date())
-//         })
-//         .then(
-//           (snap2) => {
-//             var item = {};
-//             item["qna_last_index"] = index + 1;
-//             snap.ref.set(item, { merge: true }).then((x) => {
-//               response.status(200).send({
-//                 success: true,
-//               });
-//             });
-//           },
-//           (reason) => {
-//             response.status(405).send(reason);
-//           }
-//         );
-//     });
-//   });
-// });
-
-exports.editQna = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    ////console.log("get edit qna");
+exports.editData = functions.https.onRequest((request, response) => {
+  return cors(request, response, () => {
     var body = request.body;
-    ////console.log("index: ", body.index);
-    var index = body.index;
+    var data=body.data;
+    var collectionName="";
 
-    var qnasref = db.collection("qnas");
-    var query = qnasref.where("index", "==", index);
+    if(body.type == 'qna'){
+      collectionName = "qnas"
+    }
+    else if(body.type == 'notice'){
+      collectionName = "notices"
+    }
+    else if(body.type == 'schedule'){
+      collectionName="schedules"
+    }
+    else{
+      response.status(405).send({
+        message : 'write data type ! '
+      });
+    }
+
+    var index = Number(data.index);
+
+    var noticesref = db.collection(collectionName);
+    var query = noticesref.where("index", "==", index);
     return query.get().then((snap) => {
-      snap.docs[0].ref.set(body.newData, { merge: true }).then(
+      snap.docs[0].ref.set(data.newData, { merge: true }).then(
         (snap2) => {
           response.status(200).send({
             success: true,
           });
         },
         (reason) => {
-          ////console.log("set error");
           response.status(405).send(reason);
         }
       );
     });
   });
 });
-
-exports.editNotice = functions.https.onRequest((request, response) => {
-    cors(request, response, () => {
-      ////console.log("get edit notice");
-      var body = request.body;
-      ////console.log("index: ", body.index);
-      var index = Number(body.index);
-  
-      var noticesref = db.collection("notices");
-      var query = noticesref.where("index", "==", index);
-      return query.get().then((snap) => {
-          ////console.log(snap)
-        snap.docs[0].ref.set(body.newData, { merge: true }).then(
-          (snap2) => {
-            response.status(200).send({
-              success: true,
-            });
-          },
-          (reason) => {
-            ////console.log("set error");
-            response.status(405).send(reason);
-          }
-        );
-      });
-    });
-  });
-
-// exports.postNotice = functions.https.onRequest((request, response) => {
-//   cors(request, response, () => {
-//     var body = request.body;
-//     var getCurrentMetadataPromise = db
-//       .doc("metadata/PpmSaO7W089QJuMBY9Md")
-//       .get();
-//     getCurrentMetadataPromise.then((snap) => {
-//       var index = snap.data()["notice_last_index"];
-//       var noticeRef = db.collection("notices");
-//       //console.log("try set");
-//       return noticeRef
-//         .doc()
-//         .set({
-//           index: index + 1,
-//           title: body.title,
-//           category: body.category,
-//           content: body.content,
-//           timestamp: Date.now(),
-//         })
-//         .then(
-//           (snap2) => {
-//             //console.log("try notice update");
-//             var item = {};
-//             item["notice_last_index"] = index + 1;
-//             snap.ref.set(item, { merge: true }).then((x) => {
-//               response.status(200).send({
-//                 success: true,
-//               });
-//             });
-//           },
-//           (reason) => {
-//             //console.log("set error");
-//             response.status(405).send(reason);
-//           }
-//         );
-//     });
-//   });
-// });
-
-// exports.getQnaIndexs = functions.https.onRequest((request, response) => {
-//   cors(request, response, () => {
-//     var getCurrentMetadataPromise = db
-//       .doc("metadata/PpmSaO7W089QJuMBY9Md")
-//       .get();
-//     return getCurrentMetadataPromise.then((snap) => {
-//       var index = snap.data()["qna_last_index"];
-//       //console.log("get current qna indexs(count) = ", index);
-//       response.status(200).send({
-//         current_index: index,
-//       });
-//     });
-//   });
-// });
 
 exports.getCurrentLastIndex= functions.https.onRequest((request, response) => {
   return cors(request, response, () => {
@@ -303,17 +214,6 @@ exports.getCurrentLastIndex= functions.https.onRequest((request, response) => {
     });
   });
 });
-
-// exports.getNoticeIndexs = functions.https.onRequest((request, response) => {
-//   cors(request, response, () => {
-//     return GetCurrentNoticeHighestIndex().then((index) => {
-//       //console.log("get current notice indexs(count) = ", index);
-//       response.status(200).send({
-//         current_index: index,
-//       });
-//     });
-//   });
-// });
 
 exports.getDatasByIndexs = functions.https.onRequest((request, response) => {
   return cors(request, response, () => {
@@ -375,50 +275,6 @@ exports.getDatasByIndexs = functions.https.onRequest((request, response) => {
   });
 });
 
-// exports.getQnasByIndexs = functions.https.onRequest((request, response) => {
-//   cors(request, response, () => {
-//     var from = Number(request.query.from);
-//     var to = Number(request.query.to);
-//     return GetCurrentQnasHighestIndex().then((index) => {
-//       if (from > to) {
-//         response
-//           .status(400)
-//           .send({ message: "from param was bigger than to param" });
-//         return;
-//       }
-
-//       if (from > index) {
-//         response
-//           .status(400)
-//           .send({ message: "from param was bigger than whole indexes" });
-//         return;
-//       }
-
-//       if (from < 0) {
-//         response
-//           .status(400)
-//           .send({ message: "from param was smaller than zero" });
-//         return;
-//       }
-
-//       var qnasRef = db.collection("qnas");
-//       qnasRef
-//         .where("index", ">=", from)
-//         .where("index", "<", to)
-//         .get()
-//         .then(
-//           (snap) => {
-//             response.status(200).send(snap.docs.map((doc) => doc.data()));
-//           },
-//           (reason) => {
-//             //console.log("query error");
-//             response.status(404).send(reason);
-//           }
-//         );
-//     });
-//   });
-// });
-
 exports.getWritingsByCategory = functions.https.onRequest((request, response) => {
   return cors(request, response, () => {
     var type = request.query.type;
@@ -450,96 +306,37 @@ exports.getWritingsByCategory = functions.https.onRequest((request, response) =>
   });
 });
 
-// exports.getQnasByCategory = functions.https.onRequest((request, response) => {
-//   cors(request, response, () => {
-//     //console.log("query : ", request.query);
-//     var category = request.query.category;
-//     return GetCurrentQnasHighestIndex().then((index) => {
-//       var qnasRef = db.collection("qnas");
-//       qnasRef
-//         .where("category", "==", category)
-//         .get()
-//         .then(
-//           (snap) => {
-//             response.status(200).send(snap.docs.map((doc) => doc.data()));
-//           },
-//           (reason) => {
-//             //console.log("query error");
-//             response.status(404).send(reason);
-//           }
-//         );
-//     });
-//   });
-// });
+exports.getSchedulesByMonth= functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    var year = Number(request.query.year);
+    var month = Number(request.query.month);
+    var collectionName="schedules"
+    var qnasRef = db.collection(collectionName);
 
-// exports.getNoticesByCategory = functions.https.onRequest(
-//   (request, response) => {
-//     cors(request, response, () => {
-//       //console.log("query : ", request.query);
-//       var category = request.query.category;
-//       return GetCurrentNoticeHighestIndex().then((index) => {
-//         var qnasRef = db.collection("notices");
-//         qnasRef
-//           .where("category", "==", category)
-//           .get()
-//           .then(
-//             (snap) => {
-//               response.status(200).send(snap.docs.map((doc) => doc.data()));
-//             },
-//             (reason) => {
-//               //console.log("query error");
-//               response.status(404).send(reason);
-//             }
-//           );
-//       });
-//     });
-//   }
-// );
+    if(year ==0 || month == 0){
+      response.status(405).send({
+        message : 'write right data type ! '
+      });
+      return
+    }
 
-// exports.getNoticesByIndexs = functions.https.onRequest((request, response) => {
-//   cors(request, response, () => {
-//     //console.log("query : ", request.query);
-//     var from = Number(request.query.from);
-//     var to = Number(request.query.to);
-//     return GetCurrentNoticeHighestIndex().then((index) => {
-//       if (from > to) {
-//         response
-//           .status(400)
-//           .send({ message: "from param was bigger than to param" });
-//         return;
-//       }
-
-//       if (from > index) {
-//         response
-//           .status(400)
-//           .send({ message: "from param was bigger than whole indexes" });
-//         return;
-//       }
-
-//       if (from < 0) {
-//         response
-//           .status(400)
-//           .send({ message: "from param was smaller than zero" });
-//         return;
-//       }
-
-//       var qnasRef = db.collection("notices");
-//       qnasRef
-//         .where("index", ">=", from)
-//         .where("index", "<", to)
-//         .get()
-//         .then(
-//           (snap) => {
-//             response.status(200).send(snap.docs.map((doc) => doc.data()));
-//           },
-//           (reason) => {
-//             //console.log("query error");
-//             response.status(404).send(reason);
-//           }
-//         );
-//     });
-//   });
-// });
+    var fromMonth=new Date(year,month-1)
+    var toMonth=new Date(year, month)
+    console.log('fromMonth: ',fromMonth,'toMonth: ',toMonth)
+    // toMonth.setDate(fromMonth.getDate() + 31)
+    return qnasRef
+      .where("start", ">=", fromMonth).where("start","<=",toMonth)
+      .get()
+      .then(
+        (snap) => {
+          response.status(200).send(snap.docs.map((doc) => doc.data()));
+        },
+        (reason) => {
+          response.status(404).send(reason);
+        }
+      );
+  });
+});
 
 function GetCurrentQnasHighestIndex() {
   var getCurrentMetadataPromise = db.doc("metadata/PpmSaO7W089QJuMBY9Md").get();
--- backend/public/index.js
+++ backend/public/index.js
@@ -1,5 +1,5 @@
 function clickBtnSendMessage(){
-    var msgbox = document.getElementById('msg')
+    var msgbox = document.getElementById('pushmsg')
     var msg=msgbox.value
     fetch("https://us-central1-mme-info.cloudfunctions.net/apis-sendMessageStringOnly",{
       "method" : "POST",
@@ -12,8 +12,64 @@ function clickBtnSendMessage(){
       "body" : JSON.stringify({
         "message" : msg
       })
-    }).then((response)=> response.text()).then((resptext) =>{
-      console.log('get response ', resptext)
-      alert("get response : ", resptext)
+    }).then((response)=> response.json()).then((respjson) =>{
+      console.log('get response ', respjson)
+      alert("get response : ", respjson)
     })
   }
+
+
+function clickBtnAdd(){
+    var addName = document.getElementById('addName').value
+    var addMonthFrom = document.getElementById('addMonthFrom').value
+    var addMonthTo = document.getElementById('addMonthTo').value
+    var startDate=new Date(addMonthFrom)
+    var endDate=new Date(addMonthTo)
+    fetch("https://us-central1-mme-info.cloudfunctions.net/apis-postData",{
+      "method" : "POST",
+      "mode": "no-cors", 
+      "cache": "no-cache", 
+      "credentials": "same-origin", 
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "body" : JSON.stringify({
+        "type" : "schedule",
+        "data":{
+            "start" : startDate,
+            "end" : endDate,
+            "name" : addName
+        }
+      })
+    }).then((response)=> {
+        console.log('response : ', response)
+        return response.json()
+    }).then((respjson) =>{
+      console.log('get response ', respjson)
+      alert("get response : ", respjson)
+    })
+}
+
+function clickBtnSearch(){
+    var searchYear=document.getElementById('searchYear').value
+    var searchMonth = document.getElementById('searchMonth').value
+    if(searchMonth.length == 1){
+        searchMonth = "0"+searchMonth
+    }
+    fetch("https://us-central1-mme-info.cloudfunctions.net/apis-getSchedulesByMonth/?year="+searchYear+"-"+searchMonth,{
+      "method" : "GET",
+      "mode": "no-cors", 
+      "cache": "no-cache", 
+      "credentials": "same-origin"
+    }).then((response)=> {
+        console.log('response : ', response)
+        return response.json()
+    }).then((respjson) =>{
+      console.log('get response ', respjson)
+      //alert("get response : ", respjson)
+    })
+}
+
+function changeScheduleMode(schedule){
+
+}
