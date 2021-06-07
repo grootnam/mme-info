4459350d3933210e429821e548f250b97e7c0c96
yu4321 <ryu.youngseok0512@gmail.com>
2021-06-08 04:21:34 +0900

    댓글 작성까지 완성

--- CordovaTest/mmeinfo/www/Q&A/Q&A.js
+++ CordovaTest/mmeinfo/www/Q&A/Q&A.js
@@ -1,22 +1,95 @@
-qna_comment = [
-    {
-        type : "etc",
-        title : "아마 베스킨라빈스 주지 않을까요?",
-        index : 2232
-    },
-    {
-        type : "etc",
-        title : "맘스터치!",
-        index : 223232
-    },
-    {
-        type : "etc",
-        title : "서브웨이 기프티콘",
-        index : 213122
-    },
-    {
-        type : "etc",
-        title : "상록원 식사권",
-        index : 21322
+
+$(document).delegate(".ui-page", "pageshow", function () {
+  makeQnaPage();
+});
+
+currentObj=undefined
+function sendComment(){
+    var comment=document.getElementById("comment").value
+    if(comment.length<=0){
+        confirm("댓글은 1자 이상 작성해주십시오.")
     }
-  ]
+
+    fetch("https://us-central1-mme-info.cloudfunctions.net/apis-postData",{
+        "method" : "POST",
+      "mode": "no-cors", 
+      "cache": "no-cache", 
+      "credentials": "same-origin", 
+      "headers": {
+        "Content-Type": "application/json"
+      },
+        "body" : JSON.stringify({
+          "type" : "comment",
+          "data" :{
+              "text" : comment,
+              "comment_index" : Number(currentObj["index"]),
+              "comment_type" : "qna"
+          }
+        })
+      }).then((response)=>{
+          makeQnaPage()
+      })
+}
+
+function makeQnaPage() {
+  var cur = localStorage.getItem("selectedQnA");
+  if (cur.length <= 0) {
+    console.log("qna not selected");
+    return;
+  }
+  var obj = JSON.parse(cur);
+  if (!obj) {
+    console.log("obj unparsable : ", cur);
+    return;
+  }
+
+  currentObj=obj
+
+  var title = document.getElementById("title");
+  var content = document.getElementById("content");
+  var comment_container=document.getElementById("comment_container")
+
+
+  title.innerText = "";
+  content.innerText = "";
+
+  title.innerText = obj["title"];
+  content.innerText = obj["content"];
+
+  while(comment_container.firstChild){
+    comment_container.removeChild(comment_container.firstChild)
+    }
+  var idx = obj["index"];
+
+  fetch(
+    "https://us-central1-mme-info.cloudfunctions.net/apis-getCommentsByTypeAndIndex?type=qna&index=" +
+      idx
+  )
+    .then((response) => response.json())
+    .then((datas) => {
+        var qna_comment = [];
+        datas.forEach(element => {
+            qna_comment.push({
+                index : Number(element["index"]),
+                text : element["text"]
+            })
+        });
+        qna_comment = qna_comment.sort((a,b)=>{
+            return a["index"] - b["index"]
+        })
+
+        qna_comment.forEach((element)=>{
+            var span = document.createElement("span");
+            var div1= document.createElement("div")
+            var div2=document.createElement("div")
+            span.classList.add("comment")
+            div1.classList.add("comment_head")
+            div2.classList.add("comment_tail")
+            div1.innerText="A"
+            div2.innerText=element["text"]
+            span.appendChild(div1)
+            span.appendChild(div2)
+            comment_container.appendChild(span)
+        })
+    });
+}
--- CordovaTest/mmeinfo/www/Q&As/Q&As.js
+++ CordovaTest/mmeinfo/www/Q&As/Q&As.js
@@ -34,58 +34,132 @@ qnas=[]
 
 window.onload = makePage
 
-
-function makeCertainPage(categoryName){
-  var container= document.getElementById('content_container')
-  while(container.firstChild){
-    container.removeChild(container.firstChild)
+function makeCertainPage(categoryName) {
+  var container = document.getElementById("content_container");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
   }
-  console.log('container cleared');
-  for( i=0;i<qnas.length;i++){
-    var element = qnas[i]
+  console.log("container cleared");
+  qnas.forEach((element) => {
     if(element.type != categoryName){
-      continue
+      return
     }
-    var newone = document.createElement('span')
-    newone.classList.add('content_'+element.type)
-    var div=document.createElement('div')
-    div.innerText="Q"
-    div.classList.add('content_head', 'fontB')
-    var a = document.createElement('a')
-    a.classList.add('content_tail')
-    a.innerHTML = element.title
-    a.href=element.index
+    var newone = document.createElement("span");
+    newone.classList.add("content_" + element.type);
+    var div = document.createElement("div");
+    div.innerText = "Q";
+    div.classList.add("content_head");
+    var a = document.createElement("a");
+    a.classList.add("content_tail");
+    a.innerHTML = element.title;
+    a.onclick = function () {
+      var cur = element;
+      console.log("cur object : ", cur);
+      localStorage.setItem("selectedQnA", JSON.stringify(cur));
+      window.location.href = "../Q&A/Q&A.html";
+    };
 
     newone.appendChild(div);
     newone.appendChild(a);
-    container.appendChild(newone)
-  };
-
-  console.log('container populated', container.childNodes.length)
-}
-
-function makePage(){
-  var container= document.getElementById('content_container')
-  while(container.firstChild){
-    container.removeChild(container.firstChild)
-  }
-  console.log('container cleared');
-  qnas.forEach(element => {
-    var newone = document.createElement('span')
-    newone.classList.add('content_'+element.type)
-    var div=document.createElement('div')
-    div.innerText="Q"
-    div.classList.add('content_head', 'fontB')
-    var a = document.createElement('a')
-    a.classList.add('content_tail')
-    a.innerHTML = element.title
-    a.href=element.index
-
-    newone.appendChild(div);
-    newone.appendChild(a);
-    container.appendChild(newone)
+    container.appendChild(newone);
   });
+  console.log("container populated", container.childNodes.length);
+
 }
+
+function makePage() {
+  var container = document.getElementById("content_container");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  console.log("container cleared");
+  fetch(
+    "https://us-central1-mme-info.cloudfunctions.net/apis-getDatasByIndexs?type=qna&from=0&to=100"
+  )
+    .then((response) => response.json())
+    .then((datas) => {
+      qnas = [];
+      datas.forEach((element) => {
+        qnas.push({
+          type: element["category"],
+          title: element["title"],
+          content: element["content"],
+          index: element["index"]
+        });
+      });
+      qnas.forEach((element) => {
+        var newone = document.createElement("span");
+        newone.classList.add("content_" + element.type);
+        var div = document.createElement("div");
+        div.innerText = "Q";
+        div.classList.add("content_head");
+        var a = document.createElement("a");
+        a.classList.add("content_tail");
+        a.innerHTML = element.title;
+        a.onclick = function () {
+          var cur = element;
+          console.log("cur object : ", cur);
+          localStorage.setItem("selectedQnA", JSON.stringify(cur));
+          window.location.href = "../Q&A/Q&A.html";
+        };
+
+        newone.appendChild(div);
+        newone.appendChild(a);
+        container.appendChild(newone);
+      });
+    });
+}
+// function makeCertainPage(categoryName){
+//   var container= document.getElementById('content_container')
+//   while(container.firstChild){
+//     container.removeChild(container.firstChild)
+//   }
+//   console.log('container cleared');
+//   for( i=0;i<qnas.length;i++){
+//     var element = qnas[i]
+//     if(element.type != categoryName){
+//       continue
+//     }
+//     var newone = document.createElement('span')
+//     newone.classList.add('content_'+element.type)
+//     var div=document.createElement('div')
+//     div.innerText="Q"
+//     div.classList.add('content_head', 'fontB')
+//     var a = document.createElement('a')
+//     a.classList.add('content_tail')
+//     a.innerHTML = element.title
+//     a.href=element.index
+
+//     newone.appendChild(div);
+//     newone.appendChild(a);
+//     container.appendChild(newone)
+//   };
+
+//   console.log('container populated', container.childNodes.length)
+// }
+
+// function makePage(){
+//   var container= document.getElementById('content_container')
+//   while(container.firstChild){
+//     container.removeChild(container.firstChild)
+//   }
+//   console.log('container cleared');
+//   qnas.forEach(element => {
+//     var newone = document.createElement('span')
+//     newone.classList.add('content_'+element.type)
+//     var div=document.createElement('div')
+//     div.innerText="Q"
+//     div.classList.add('content_head', 'fontB')
+//     var a = document.createElement('a')
+//     a.classList.add('content_tail')
+//     a.innerHTML = element.title
+//     a.href=element.index
+
+//     newone.appendChild(div);
+//     newone.appendChild(a);
+//     container.appendChild(newone)
+//   });
+// }
 
 function openMenu(evt, menuName) {
  
